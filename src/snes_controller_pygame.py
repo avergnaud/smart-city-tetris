8c8da0df0012194df8932dc6f1ed3aaede22d56d
Adrien Vergnaud <a.vergnaud@gmail.com>
2019-01-20 12:44:54 +0100

    first

---
+++ src/snes_controller_pygame.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+print("Joystics: ", pygame.joystick.get_count())
+my_joystick = pygame.joystick.Joystick(0)
+my_joystick.init()
+clock = pygame.time.Clock()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.JOYAXISMOTION:
+            if my_joystick.get_axis(0) < -0.5 and my_joystick.get_axis(1) == 0:
+                print("gauche")
+            elif my_joystick.get_axis(0) > 0.5 and my_joystick.get_axis(1) == 0:
+                print("droite")
+            elif my_joystick.get_axis(0) == 0 and my_joystick.get_axis(1) > 0.5:
+                print("bas")
+            elif my_joystick.get_axis(0) == 0 and my_joystick.get_axis(1) < -0.5:
+                print("haut")
+        clock.tick(40)
+
+pygame.quit ()
