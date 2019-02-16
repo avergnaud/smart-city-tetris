0abf535ee4fd3dd3a21473bb56aa2dca88147831
Adrien Vergnaud <a.vergnaud@gmail.com>
2019-02-16 20:47:18 +0100

    iota watcher

--- src/wallet_watcher.py
+++ src/wallet_watcher.py
@@ -3,9 +3,37 @@ une autre solution est d'utiliser ZMQ
 MAIS, il faut identifier un full node qui offre un feed ZMQ
 tcp://zmq.devnet.iota.org:5556 n'est pas stable...
 '''
-from iota import Iota
+import time
+
+from iota import Iota, TryteString
+from iota import Tag, Transaction, TransactionHash
 
 api = Iota("https://nodes.devnet.thetangle.org:443")
-print(api.get_node_info())
+
 transactions = api.find_transactions(addresses=["GHLEMUXUIYJK9SKXOUMNEKZBRDHZVFUURXOYMQQODSWEGYRROOGGILVYTGTCCLOYDCETCKHUT9LRXJMMD"])
-print(transactions)
+transactions_hashes = transactions['hashes']
+#print(len(transactions_hashes), ' transactions_hashes')
+
+while True:
+    # Code executed here
+    retrieved_transactions = api.find_transactions(
+        addresses=["GHLEMUXUIYJK9SKXOUMNEKZBRDHZVFUURXOYMQQODSWEGYRROOGGILVYTGTCCLOYDCETCKHUT9LRXJMMD"])
+    retrieved_transactions_hashes = retrieved_transactions['hashes']
+
+    #print(len(retrieved_transactions_hashes), ' retrieved_transactions_hashes')
+
+    new_transactions_hashes = [tx for tx in retrieved_transactions_hashes if tx not in transactions_hashes]
+
+    #print(len(new_transactions_hashes), ' new_transactions_hashes')
+
+    if len(new_transactions_hashes) != 0:
+        transactions_hashes += new_transactions_hashes
+
+        new_transactions_hashes_trytes = api.get_trytes(new_transactions_hashes)
+        for tryte in new_transactions_hashes_trytes['trytes']:
+            tx = Transaction.from_tryte_string(tryte)
+            print(TryteString(tx.tag).decode())
+            print(TryteString(tx.signature_message_fragment).decode())
+
+    time.sleep(5)
+
